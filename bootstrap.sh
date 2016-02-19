eea11a2f90156aa7634d31f9ca4936b6759fc837
Meesayen <gvg.fede@gmail.com>
2016-02-19 20:46:59 +0000

    Added my customizations

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,18 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
+sh brew.sh;
+
+if ! grep '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
+
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" \
+		--exclude ".osx" --exclude "brew.sh" --exclude "bin/" \
+		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
@@ -20,3 +29,15 @@ else
 	fi;
 fi;
 unset doIt;
+
+# Install latest nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+
+sh .osx;
+
+echo "\n\nDone. Note that some of these changes require a logout/restart to take effect."
+echo "Remember to:\n"
+echo "  nvm install 5"
+echo "  nvm alias default 5"
+echo "  npm i -g npm"
+echo "  npm i -g pnpm diff-so-fancy"
